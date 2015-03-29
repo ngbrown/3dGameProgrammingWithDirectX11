f31f1f9037711346fb7a036c5abd9c64761c2f93
Nathan Brown <nathan@nkbrown.us>
2015-03-29 00:18:01 -0700

    Vectors - Listing 1.6.9

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -91,6 +91,31 @@ void VectorFunctions()
 	cout << "angle               = " << angleDegrees << endl;
 }
 
+void FloatingPointError()
+{
+	cout.precision(8);
+
+	XMVECTOR u = XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f);
+	XMVECTOR n = XMVector3Normalize(u);
+
+	float LU = XMVectorGetX(XMVector3Length(n));
+
+	// Mathematically, the length should be 1. Is it numerically?
+	cout << LU << endl;
+	if (LU == 1.0f)
+	{
+		cout << "Length 1" << endl;
+	}
+	else
+	{
+		cout << "Length not 1" << endl;
+	}
+
+	// Raising 1 to any power should still be 1. Is it?
+	float powLU = powf(LU, 1.0e6f);
+	cout << "LU^(10^6) = " << powLU << endl;
+}
+
 int main()
 {
 	cout.setf(ios_base::boolalpha);
@@ -105,4 +130,6 @@ int main()
 	SetterFunctions();
 	cout << endl;
 	VectorFunctions();
+	cout << endl;
+	FloatingPointError();
 }
