d0018acfb5ace4b0e4c8f62fe39c28ddb83f6af2
Nathan Brown <nathan@nkbrown.us>
2015-03-29 00:03:29 -0700

    Vectors - Listing 1.6.8

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <Windows.h> // for FLOAT definition
+﻿#include <Windows.h> // for correct XMVerifyCPUSupport() operation
 #include <DirectXMath.h>
 #include <iostream>
 
@@ -16,17 +16,8 @@ ostream& operator<<(ostream& os, FXMVECTOR v)
 	return os;
 }
 
-int main()
+void SetterFunctions()
 {
-	cout.setf(ios_base::boolalpha);
-
-	// Check support for SSE2 (Pentium4, AMD K8, and above).
-	if (!XMVerifyCPUSupport())
-	{
-		cout << "xna math not supported" << endl;
-		return 0;
-	}
-
 	XMVECTOR p = XMVectorZero();
 	XMVECTOR q = XMVectorSplatOne();
 	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
@@ -38,4 +29,80 @@ int main()
 	cout << "u = " << u << endl;
 	cout << "v = " << v << endl;
 	cout << "w = " << w << endl;
+}
+
+void VectorFunctions()
+{
+	XMVECTOR n = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
+	XMVECTOR v = XMVectorSet(-2.0f, 1.0f, -3.0f, 0.0f);
+	XMVECTOR w = XMVectorSet(0.707f, 0.707f, 0.0f, 0.0f);
+
+	// Vector addition XMVECTOR operator +
+	XMVECTOR a = u + v;
+	// Vector subtraction: XMVECTOR operator -
+	XMVECTOR b = u - v;
+
+	// Scalar multiplication: XMVECTOR operator *
+	XMVECTOR c = 10.0f*u;
+
+	// ||u||
+	XMVECTOR L = XMVector3Length(u);
+
+	// d = u / ||u||
+	XMVECTOR d = XMVector3Normalize(u);
+
+	// s = u dot v
+	XMVECTOR s = XMVector3Dot(u, v);
+
+	// e = u x v
+	XMVECTOR e = XMVector3Cross(u, v);
+
+	// Find proj_n(w) and perp_n(w)
+	XMVECTOR projW;
+	XMVECTOR perpW;
+	XMVector3ComponentsFromNormal(&projW, &perpW, w, n);
+
+	// Does projW + perpW == w?
+	bool equal = XMVector3Equal(projW + perpW, w) != 0;
+	bool notEqual = XMVector3NotEqual(projW + perpW, w) != 0;
+
+	// The angnle between projW and perpW should be 90 degrees.
+	XMVECTOR angleVec = XMVector3AngleBetweenVectors(projW, perpW);
+	float angleRadians = XMVectorGetX(angleVec);
+	float angleDegrees = XMConvertToDegrees(angleRadians);
+
+	XMVector3ComponentsFromNormal(&projW, &perpW, w, n);
+
+	cout << "u                   = " << u << endl;
+	cout << "v                   = " << v << endl;
+	cout << "w                   = " << w << endl;
+	cout << "n                   = " << n << endl;
+	cout << "a = u + v           = " << a << endl;
+	cout << "b = u - v           = " << b << endl;
+	cout << "c = 10 * u          = " << c << endl;
+	cout << "L = ||u||           = " << L << endl;
+	cout << "s = u dot v         = " << s << endl;
+	cout << "projW               = " << projW << endl;
+	cout << "perpW               = " << perpW << endl;
+
+	cout << "projW + perpW == w  = " << equal << endl;
+	cout << "projW + perpW != w  = " << notEqual << endl;
+	cout << "angle               = " << angleDegrees << endl;
+}
+
+int main()
+{
+	cout.setf(ios_base::boolalpha);
+
+	// Check support for SSE2 (Pentium4, AMD K8, and above).
+	if (!XMVerifyCPUSupport())
+	{
+		cout << "xna math not supported" << endl;
+		return 0;
+	}
+
+	SetterFunctions();
+	cout << endl;
+	VectorFunctions();
 }
