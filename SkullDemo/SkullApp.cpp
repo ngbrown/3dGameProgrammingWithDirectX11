fb2eb8c69835bd0663e0ddb475178af8641970dc
Nathan Brown <nathan@nkbrown.us>
2015-04-19 09:57:50 -0700

    SkullDemo - model loading demo 6.12

---
+++ SkullDemo/SkullApp.cpp
@@ -0,0 +1,373 @@
+﻿//***************************************************************************************
+// SkullDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates loading more complicated geometry from file and rendering it.
+//
+// Controls:
+//		Hold the left mouse button down and move the mouse to rotate.
+//      Hold the right mouse button down to zoom in and out.
+//
+//***************************************************************************************
+
+#include <d3dApp.h>
+#include <MathHelper.h>
+#include <ConstantBuffer.h>
+#include <ShaderHelper.h>
+
+#include <GeometryGenerator.h>
+
+#include "cbPerObject.h"
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+// Not to be confused with GeometryGenerator.Vertex
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+class SkullApp : public D3DApp
+{
+public:
+	SkullApp(HINSTANCE hInstance);
+	~SkullApp();
+
+	bool Init();				// override
+	void OnResize();			// override
+	void UpdateScene(float dt);	// implement pure virtual
+	void ApplyWorldViewProj(const XMMATRIX& worldViewProj);
+	void DrawScene(); 			// implement pure virtual
+
+	void OnMouseDown(WPARAM btnState, int x, int y);	// override
+	void OnMouseUp(WPARAM btnState, int x, int y);		// override
+	void OnMouseMove(WPARAM btnState, int x, int y);	// override
+
+private:
+	void BuildGeometryBuffers();
+	void BuildFX();
+	void BuildVertexLayout();
+	void BuildRasterState();
+
+private:
+	ConstantBuffer<cbPerObject> mObjectConstantBuffer;
+	ID3D11Buffer* mVB;
+	ID3D11Buffer* mIB;
+	ID3DBlob* mPSBlob;
+	ID3DBlob* mVSBlob;
+	ID3D11PixelShader* mPixelShader;
+	ID3D11VertexShader* mVertexShader;
+
+	ID3D11RasterizerState* mRasterState;
+	ID3D11RasterizerState* mWireframeRS;
+
+	ID3D11InputLayout* mInputLayout;
+
+	// Define transformations from local spaces to world space.
+	XMFLOAT4X4 mSkullWorld;
+
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	UINT mSkullIndexCount;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	SkullApp theApp(hInstance);
+
+	if (!theApp.Init())
+		return 0;
+
+	return theApp.Run();
+}
+
+
+SkullApp::SkullApp(HINSTANCE hInstance)
+	: D3DApp(hInstance),
+	mVB(nullptr),
+	mIB(nullptr),
+	mRasterState(nullptr),
+	mWireframeRS(nullptr),
+	mInputLayout(nullptr),
+	mSkullIndexCount(0),
+	mTheta(1.5f*MathHelper::Pi),
+	mPhi(0.1f*MathHelper::Pi),
+	mRadius(20.0f)
+{
+	mMainWndCaption = L"Skull Demo";
+
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mView, I);
+	XMStoreFloat4x4(&mProj, I);
+
+	XMMATRIX skullOffset = XMMatrixTranslation(0.0f, -2.0f, 0.0f);
+	XMStoreFloat4x4(&mSkullWorld, skullOffset);
+}
+
+SkullApp::~SkullApp()
+{
+	ReleaseCOM(mVB);
+	ReleaseCOM(mIB);
+	ReleaseCOM(mInputLayout);
+	ReleaseCOM(mWireframeRS);
+}
+
+bool SkullApp::Init()
+{
+	if (!D3DApp::Init())
+		return false;
+
+	BuildGeometryBuffers();
+	BuildFX();
+	BuildVertexLayout();
+	BuildRasterState();
+	mObjectConstantBuffer.Initialize(md3dDevice);
+
+	return true;
+}
+
+void SkullApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	// The window resized, so update the aspect ratio and recompute the projection matrix.
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
+}
+
+void SkullApp::UpdateScene(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = mRadius*sinf(mPhi)*cosf(mTheta);
+	float z = mRadius*sinf(mPhi)*sinf(mTheta);
+	float y = mRadius*cosf(mPhi);
+
+	// Build the view matrix.
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&mView, V);
+}
+
+void SkullApp::ApplyWorldViewProj(const XMMATRIX& worldViewProj)
+{
+	// Use a constant buffer. Effect framework deprecated
+	cbPerObject mPerObjectCB;
+	XMStoreFloat4x4(&mPerObjectCB.mWorldViewProj, XMMatrixTranspose(worldViewProj));
+	mObjectConstantBuffer.Data = mPerObjectCB;
+	mObjectConstantBuffer.ApplyChanges(md3dImmediateContext);
+	ID3D11Buffer* buffer = mObjectConstantBuffer.Buffer();
+	md3dImmediateContext->VSSetConstantBuffers(0, 1, &buffer);
+}
+
+void SkullApp::DrawScene()
+{
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	md3dImmediateContext->IASetInputLayout(mInputLayout);
+	md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	md3dImmediateContext->RSSetState(mWireframeRS);
+
+	// Set vertex and pixel shaders
+	md3dImmediateContext->PSSetShader(mPixelShader, nullptr, 0);
+	md3dImmediateContext->VSSetShader(mVertexShader, nullptr, 0);
+
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	md3dImmediateContext->IASetVertexBuffers(0, 1, &mVB, &stride, &offset);
+	md3dImmediateContext->IASetIndexBuffer(mIB, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set constants
+	XMMATRIX view = XMLoadFloat4x4(&mView);
+	XMMATRIX proj = XMLoadFloat4x4(&mProj);
+	XMMATRIX world = XMLoadFloat4x4(&mSkullWorld);
+	XMMATRIX worldViewProj = world*view*proj;
+
+	ApplyWorldViewProj(worldViewProj);
+	md3dImmediateContext->DrawIndexed(mSkullIndexCount, 0, 0);
+
+	HR(mSwapChain->Present(0, 0));
+}
+
+void SkullApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void SkullApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void SkullApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btnState & MK_RBUTTON) != 0)
+	{
+		// Make each pixel correspond to 0.01 unit in the scene.
+		float dx = 0.01f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.01f*static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 200.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void SkullApp::BuildGeometryBuffers()
+{
+	std::ifstream fin("Models/skull.txt");
+
+	if (!fin)
+	{
+		MessageBox(nullptr, L"Models/skull.txt not found.", nullptr, 0);
+		return;
+	}
+
+	UINT vcount = 0;
+	UINT tcount = 0;
+	std::string ignore;
+
+	fin >> ignore >> vcount;
+	fin >> ignore >> tcount;
+	fin >> ignore >> ignore >> ignore >> ignore;
+
+	float nx, ny, nz;
+	XMFLOAT4 black(0.0f, 0.0f, 0.0f, 1.0f);
+
+	std::vector<Vertex> vertices(vcount);
+	for (UINT i = 0; i < vcount; i++)
+	{
+		fin >> vertices[i].Pos.x >> vertices[i].Pos.y >> vertices[i].Pos.z;
+
+		vertices[i].Color = black;
+
+		// Normal not used in this demo.
+		fin >> nx >> ny >> nz;
+	}
+
+	fin >> ignore;
+	fin >> ignore;
+	fin >> ignore;
+
+	mSkullIndexCount = 3 * tcount;
+	std::vector<UINT> indices(mSkullIndexCount);
+	for (UINT i = 0; i < tcount; i++)
+	{
+		fin >> indices[i * 3 + 0] >> indices[i * 3 + 1] >> indices[i * 3 + 2];
+	}
+
+	fin.close();
+
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex) * vcount;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = &vertices[0];
+	HR(md3dDevice->CreateBuffer(&vbd, &vinitData, &mVB));
+
+	//
+	// Pack the indices of all the meshes into one index buffer.
+	//
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * mSkullIndexCount;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = &indices[0];
+	HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mIB));
+}
+
+void SkullApp::BuildFX()
+{
+	// Load cso files and create shaders
+	HR(ShaderHelper::LoadCompiledShader("SimplePixelShader.cso", &mPSBlob));
+	HR(md3dDevice->CreatePixelShader(mPSBlob->GetBufferPointer(), mPSBlob->GetBufferSize(), nullptr, &mPixelShader));
+
+	HR(ShaderHelper::LoadCompiledShader("SimpleVertexShader.cso", &mVSBlob));
+	HR(md3dDevice->CreateVertexShader(mVSBlob->GetBufferPointer(), mVSBlob->GetBufferSize(), nullptr, &mVertexShader));
+}
+
+void SkullApp::BuildVertexLayout()
+{
+	// Create the vertex input layout.
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
+
+	// Create the input layout
+	HR(md3dDevice->CreateInputLayout(vertexDesc, 2, mVSBlob->GetBufferPointer(),
+		mVSBlob->GetBufferSize(), &mInputLayout));
+}
+
+void SkullApp::BuildRasterState()
+{
+	D3D11_RASTERIZER_DESC rs;
+	memset(&rs, 0, sizeof(rs));
+	rs.FillMode = D3D11_FILL_SOLID;
+	rs.CullMode = D3D11_CULL_BACK;
+	rs.AntialiasedLineEnable = rs.DepthClipEnable = true;
+	mRasterState = nullptr;
+	HR(md3dDevice->CreateRasterizerState(&rs, &mRasterState));
+
+	D3D11_RASTERIZER_DESC wireframeDesc;
+	ZeroMemory(&wireframeDesc, sizeof(D3D11_RASTERIZER_DESC));
+	wireframeDesc.FillMode = D3D11_FILL_WIREFRAME;
+	wireframeDesc.CullMode = D3D11_CULL_BACK;
+	//wireframeDesc.CullMode = D3D11_CULL_NONE;
+	wireframeDesc.FrontCounterClockwise = false;
+	wireframeDesc.DepthClipEnable = true;
+	HR(md3dDevice->CreateRasterizerState(&wireframeDesc, &mWireframeRS));
+}
